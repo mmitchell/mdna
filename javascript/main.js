a44b6d21a82643bcdedb8269acc51c77e9fcb1a2
Michael Mitchell <mmitchell@uflavor.com>
2012-12-17 14:27:09 -0500

    refactored node position math into a function; reorganized the order of some of the function in the file

--- javascript/main.js
+++ javascript/main.js
@@ -4,6 +4,10 @@ var i = null;   // i = MIDIInput
 setTimeout(function(){ navigator.requestMIDIAccess(success, error) }, 200);
 
 //Globals (I know...)
+var WIDTH = 600;
+var HEIGHT = 400;
+var RADIUS = 150;
+var NODE_COLOR = "#1090B3";
 var keys_down = new Array(88).join('0').split('').map(parseFloat)
 var intervals = new Array(66).join('0').split('').map(parseFloat)
 var master_key = [0,0,0,0,0,0,0,0,0,0,0,0];
@@ -26,6 +30,10 @@ function success(access) {
       update_master_key(event);
     }
   }
+}
+
+function error(access) {
+  console.log("MIDIAccess error.");
 }
 
 function update_master_key(event){
@@ -64,6 +72,15 @@ function is_note_up(event){
   return false; // Probably a keydown
 }
 
+function is_same_note_down_elsewhere(note){
+  for(var i=note%12; i<88; i=i+12){
+    if(keys_down[i] > 0 && i != note){
+      return true;
+    }
+  }
+  return false;
+}
+
 function interval_index_mapper(note_1, note_2){
   if(note_1 > note_2){
     return note_1*12 + note_2;
@@ -83,39 +100,27 @@ function interval_color(note_1, note_2){
   return colors[dist-1];
 }
 
-function is_same_note_down_elsewhere(note){
-  for(var i=note%12; i<88; i=i+12){
-    if(keys_down[i] > 0 && i != note){
-      return true;
-    }
-  }
-  return false;
+function node_position(node_idx){
+  x = RADIUS*Math.sin(2*Math.PI*(node_idx/12)) + WIDTH/2;
+  y = -RADIUS*Math.cos(2*Math.PI*(node_idx/12)) + HEIGHT/2;
+  return {x: x, y: y};
 }
 
-function error(access) {
-  console.log("MIDIAccess error.");
-}
-
-setTimeout( function game(){
-  var WIDTH = 600;
-  var HEIGHT = 400;
-  var RADIUS = 150;
-  var NODE_COLOR = "#1090B3";
+setTimeout( function game_setup(){
   var mdna = Raphael(50, 50, WIDTH, HEIGHT);
 
   // Create the array of interval lines
   var interval_index = 0;
   for(var i=0; i<12; i++){
     for(var j=0; j<i; j++){
-      x = RADIUS*Math.sin(2*Math.PI*(i/12)) + WIDTH/2;
-      y = -RADIUS*Math.cos(2*Math.PI*(i/12)) + HEIGHT/2;
-      x2 = RADIUS*Math.sin(2*Math.PI*(j/12)) + WIDTH/2;
-      y2 = -RADIUS*Math.cos(2*Math.PI*(j/12)) + HEIGHT/2;
-      intervals[interval_index] = mdna.path("M"+x+","+y+"L"+x2+","+y2);
+      intervals[interval_index] = mdna.path("M" + node_position(i).x + 
+                                            "," + node_position(i).y + 
+                                            "L" + node_position(j).x + 
+                                            "," + node_position(j).y);
       intervals[interval_index].attr({fill: NODE_COLOR,
-                   stroke: "#ffffff",
-                   "stroke-width": 3,
-                   opacity: 0.15
+                                      stroke: "#ffffff",
+                                      "stroke-width": 3,
+                                      opacity: 0.15
       });
       interval_index++;
     }
@@ -123,9 +128,7 @@ setTimeout( function game(){
 
   // Create the array of 12 note nodes
   master_key.forEach(function(ele, idx, arr){
-    x = RADIUS*Math.sin(2*Math.PI*(idx/12));
-    y = -RADIUS*Math.cos(2*Math.PI*(idx/12));
-    arr[idx] = mdna.circle(x + WIDTH/2, y + HEIGHT/2, 20);
+    arr[idx] = mdna.circle(node_position(idx).x, node_position(idx).y, 20);
     arr[idx].data("note", "up")
             .attr({fill: NODE_COLOR,
                    stroke: "#ffffff",
